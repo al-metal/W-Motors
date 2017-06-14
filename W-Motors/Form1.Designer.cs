35936d00ed9c2ef04023f598a50e0edb5142e0d8
al-metal <al-metal@users.noreply.github.com>
2017-06-14 10:46:20 +0400

    new Proj

---
+++ W-Motors/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace W_Motors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbKeywords = new System.Windows.Forms.TextBox();
+            this.tbDescription = new System.Windows.Forms.TextBox();
+            this.tbTitle = new System.Windows.Forms.TextBox();
+            this.rtbFullText = new System.Windows.Forms.RichTextBox();
+            this.rtbMiniText = new System.Windows.Forms.RichTextBox();
+            this.btnPrice = new System.Windows.Forms.Button();
+            this.btnImages = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbKeywords
+            // 
+            this.tbKeywords.Location = new System.Drawing.Point(11, 316);
+            this.tbKeywords.Margin = new System.Windows.Forms.Padding(2);
+            this.tbKeywords.Name = "tbKeywords";
+            this.tbKeywords.Size = new System.Drawing.Size(523, 20);
+            this.tbKeywords.TabIndex = 9;
+            // 
+            // tbDescription
+            // 
+            this.tbDescription.Location = new System.Drawing.Point(11, 293);
+            this.tbDescription.Margin = new System.Windows.Forms.Padding(2);
+            this.tbDescription.Name = "tbDescription";
+            this.tbDescription.Size = new System.Drawing.Size(523, 20);
+            this.tbDescription.TabIndex = 8;
+            // 
+            // tbTitle
+            // 
+            this.tbTitle.Location = new System.Drawing.Point(11, 271);
+            this.tbTitle.Margin = new System.Windows.Forms.Padding(2);
+            this.tbTitle.Name = "tbTitle";
+            this.tbTitle.Size = new System.Drawing.Size(523, 20);
+            this.tbTitle.TabIndex = 7;
+            // 
+            // rtbFullText
+            // 
+            this.rtbFullText.Location = new System.Drawing.Point(11, 141);
+            this.rtbFullText.Margin = new System.Windows.Forms.Padding(2);
+            this.rtbFullText.Name = "rtbFullText";
+            this.rtbFullText.Size = new System.Drawing.Size(523, 130);
+            this.rtbFullText.TabIndex = 6;
+            this.rtbFullText.Text = "";
+            // 
+            // rtbMiniText
+            // 
+            this.rtbMiniText.Location = new System.Drawing.Point(11, 11);
+            this.rtbMiniText.Margin = new System.Windows.Forms.Padding(2);
+            this.rtbMiniText.Name = "rtbMiniText";
+            this.rtbMiniText.Size = new System.Drawing.Size(523, 130);
+            this.rtbMiniText.TabIndex = 5;
+            this.rtbMiniText.Text = "";
+            // 
+            // btnPrice
+            // 
+            this.btnPrice.Location = new System.Drawing.Point(539, 12);
+            this.btnPrice.Name = "btnPrice";
+            this.btnPrice.Size = new System.Drawing.Size(145, 23);
+            this.btnPrice.TabIndex = 10;
+            this.btnPrice.Text = "Обработать прайс";
+            this.btnPrice.UseVisualStyleBackColor = true;
+            // 
+            // btnImages
+            // 
+            this.btnImages.Location = new System.Drawing.Point(539, 41);
+            this.btnImages.Name = "btnImages";
+            this.btnImages.Size = new System.Drawing.Size(145, 23);
+            this.btnImages.TabIndex = 11;
+            this.btnImages.Text = "Обработать картинки";
+            this.btnImages.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 346);
+            this.Controls.Add(this.btnImages);
+            this.Controls.Add(this.btnPrice);
+            this.Controls.Add(this.tbKeywords);
+            this.Controls.Add(this.tbDescription);
+            this.Controls.Add(this.tbTitle);
+            this.Controls.Add(this.rtbFullText);
+            this.Controls.Add(this.rtbMiniText);
+            this.Name = "Form1";
+            this.Text = "w-motors";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbKeywords;
+        private System.Windows.Forms.TextBox tbDescription;
+        private System.Windows.Forms.TextBox tbTitle;
+        private System.Windows.Forms.RichTextBox rtbFullText;
+        private System.Windows.Forms.RichTextBox rtbMiniText;
+        private System.Windows.Forms.Button btnPrice;
+        private System.Windows.Forms.Button btnImages;
+    }
+}
+
---
+++ W-Motors/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace W_Motors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            if (!Directory.Exists("files"))
+            {
+                Directory.CreateDirectory("files");
+            }
+            if (!Directory.Exists("pic"))
+            {
+                Directory.CreateDirectory("pic");
+            }
+
+            if (!File.Exists("files\\miniText.txt"))
+            {
+                File.Create("files\\miniText.txt");
+            }
+
+            if (!File.Exists("files\\fullText.txt"))
+            {
+                File.Create("files\\fullText.txt");
+            }
+
+            if (!File.Exists("files\\title.txt"))
+            {
+                File.Create("files\\title.txt");
+            }
+
+            if (!File.Exists("files\\description.txt"))
+            {
+                File.Create("files\\description.txt");
+            }
+
+            if (!File.Exists("files\\keywords.txt"))
+            {
+                File.Create("files\\keywords.txt");
+            }
+            StreamReader altText = new StreamReader("files\\miniText.txt", Encoding.GetEncoding("windows-1251"));
+            while (!altText.EndOfStream)
+            {
+                string str = altText.ReadLine();
+                rtbMiniText.AppendText(str + "\n");
+            }
+            altText.Close();
+
+            altText = new StreamReader("files\\fullText.txt", Encoding.GetEncoding("windows-1251"));
+            while (!altText.EndOfStream)
+            {
+                string str = altText.ReadLine();
+                rtbFullText.AppendText(str + "\n");
+            }
+            altText.Close();
+
+            altText = new StreamReader("files\\title.txt", Encoding.GetEncoding("windows-1251"));
+            while (!altText.EndOfStream)
+            {
+                string str = altText.ReadLine();
+                tbTitle.AppendText(str + "\n");
+            }
+            altText.Close();
+
+            altText = new StreamReader("files\\description.txt", Encoding.GetEncoding("windows-1251"));
+            while (!altText.EndOfStream)
+            {
+                string str = altText.ReadLine();
+                tbDescription.AppendText(str + "\n");
+            }
+            altText.Close();
+
+            altText = new StreamReader("files\\keywords.txt", Encoding.GetEncoding("windows-1251"));
+            while (!altText.EndOfStream)
+            {
+                string str = altText.ReadLine();
+                tbKeywords.AppendText(str + "\n");
+            }
+            altText.Close();
+        }
+    }
+}
